09e5de04824695811e037421108fe72d5f971b7c
bzw7 <keshav_kothari@berkeley.edu>
2018-12-02 16:03:32 -0800

    cleaned prints

--- test.py
+++ test.py
@@ -7,7 +7,12 @@ import numpy as np
 from collections import Counter
 import os
 import requests, json
+from tabulate import tabulate
 from keys import consumer_key,consumer_secret,access_token,access_token_secret
+import datetime
+
+data_dir= 'data'
+name_file= 'name.npy'
 
 class TwitterClient(object): 
     ''' 
@@ -48,8 +53,7 @@ class TwitterClient(object):
                 # saving sentiment of tweet 
                 parsed_tweet['sentiment'] = get_tweet_sentiment(tweet.text) 
                 # saving gender of user
-                parsed_tweet['gender'] = get_gender(tweet.user.name.split(' ')[0]) 
-                print(tweet.user.name.split(' ')[0],parsed_tweet['gender'])
+                parsed_tweet['gender'] = get_gender(clean_word(tweet.user.name.split(' ')[0])) 
                 # appending parsed tweet to tweets list 
                 if tweet.retweet_count > 0:
                     # if tweet has retweets, ensure that it is appended only once 
@@ -86,32 +90,43 @@ def time_finder(fetched_tweets,num=10):
     return c.most_common()[:num]
 
 def get_gender(name):
-    # name = clean_tweet(name)
-    regex=re.compile('[^a-zA-Z]')
-    name=regex.sub('',name)
-    cnt = 0
-    url="name[0]="+name    
-    req = requests.get("https://api.genderize.io?" + url)
-    results = json.loads(req.text)
-    retrn = []
     try:
-        for result in results:
-            if result["gender"] is not None:
-                retrn.append((result["gender"], result["probability"], result["count"]))
-            else:
-                retrn.append((u'None',u'0.0',0.0))
+        nf=np.load(data_dir+'/'+name_file)
     except:
-        print(results["error"])
-        return 'None'
-    return retrn[0]
+        nf={}
+    if name in nf:
+        return nf[name]
+    else:
+        url="name[0]="+name    
+        req = requests.get("https://api.genderize.io?" + url)
+        results = json.loads(req.text)
+        retrn = []
+        try:
+            for result in results:
+                if result["gender"] is not None:
+                    retrn.append((result["gender"], result["probability"], result["count"]))
+                else:
+                    retrn.append((u'None',u'0.0',0.0))
+        except:
+            # print(results["error"])
+            return 'None'
+        nf[name]=retrn[0]
+        np.save(data_dir+'/'+name_file,nf)
+        return retrn[0]
 
-    
+def clean_word(word):
+    regex = re.compile('[^a-zA-Z]')
+    return regex.sub('', word)
 
 def word_finder(fetched_tweets,num=10):
     ct=[]
     for t in fetched_tweets:
-        # input(clean_tweet(t.text.lower()).split(' '))
-        ct=ct+clean_tweet(t.text.lower()).split(' ')
+        text=TextBlob(clean_tweet(t.text.lower()).replace('.',''))
+        for tt in text.tags:
+            if tt[1] not in ["WRB","DT","VBZ","CC","IN","PRP","MD"]:
+                cw=clean_word(tt[0])
+                if len(cw)>2:
+                    ct.append(cw)
     c=Counter(ct)
     return c.most_common()[:num]
 
@@ -130,7 +145,7 @@ def get_tweet_sentiment(tweet):
     else: 
         return 'negative'
 
-def main(data_dir,query,count=100,Save=True,load=False):
+def main(data_dir,query,count=100,Save=True,load=True):
     # try opening file if exists
     if load:
         try:
@@ -139,7 +154,7 @@ def main(data_dir,query,count=100,Save=True,load=False):
         except:
             load=False
     if not load:
-    # creating object of TwitterClient Class 
+        # creating object of TwitterClient Class 
         api = TwitterClient() 
         # calling function to get tweets 
         tweets,fetched_tweets = api.get_tweets(query = query, count = count)
@@ -147,42 +162,53 @@ def main(data_dir,query,count=100,Save=True,load=False):
             np.save(data_dir+'/'+query + '_' + str(count) + '_r.npy', fetched_tweets)
             np.save(data_dir+'/'+query + '_' + str(count) + '.npy', tweets)
 
-    # print(len(tweets))
-    # print(word_finder(fetched_tweets,20))
-    # print(hashtag_finder(fetched_tweets,10))
     # print(time_finder(fetched_tweets,10))
-    # # picking positive tweets from tweets 
-    # ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive'] 
-
-    # # percentage of positive tweets 
-    # print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets))) 
-    # # picking negative tweets from tweets 
-    # ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative'] 
-    # # percentage of negative tweets 
-    # print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets))) 
-    # # percentage of neutral tweets 
-    # print("Neutral tweets percentage: {} %".format(100*(len(tweets)-len(ntweets)-len(ptweets))/len(tweets))) 
+    if len(tweets)!=0:
+        # picking positive tweets from tweets 
+        ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive'] 
+        # percentage of positive tweets 
+        print("\nPositive tweets percentage: {} %".format(100*len(ptweets)/len(tweets))) 
+        # picking negative tweets from tweets 
+        ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative'] 
+        # percentage of negative tweets 
+        print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets))) 
+        # percentage of neutral tweets 
+        print("Neutral tweets percentage: {} %".format(100*(len(tweets)-len(ntweets)-len(ptweets))/len(tweets))) 
+        print('\n')
+        print(tabulate(word_finder(fetched_tweets,10),headers=['Word','Count'],tablefmt='orgtbl'))
+        print('\n')
+        print(tabulate(hashtag_finder(fetched_tweets,10),headers=['Hashtag','Count'],tablefmt='orgtbl'))
+    else:
+        print("ERROR: No Tweets Obtained")
 
     # picking female names from tweets 
     ftweets = [tweet for tweet in tweets if tweet['gender'][0] == 'female'] 
     pftweets = [tweet for tweet in ftweets if tweet['sentiment'] == 'positive']
     nftweets = [tweet for tweet in ftweets if tweet['sentiment'] == 'negative']
-    # percentage of female names
-    print("Female users percentage: {} %".format(100*len(ftweets)/len(tweets))) 
-    print("Female positive reviews percentage: {} %".format(100*len(pftweets)/len(ftweets))) 
-    print("Female negative reviews percentage: {} %".format(100*len(nftweets)/len(ftweets))) 
-    print("Female neutral reviews percentage: {} %".format(100*(len(ftweets)-len(nftweets)-len(pftweets))/len(ftweets)))
+    
+    if len(ftweets)!=0:
+        # percentage of female names
+        print("\nFemale users percentage: {} %".format(100*len(ftweets)/len(tweets))) 
+        print("Female positive reviews percentage: {} %".format(100*len(pftweets)/len(ftweets))) 
+        print("Female negative reviews percentage: {} %".format(100*len(nftweets)/len(ftweets))) 
+        print("Female neutral reviews percentage: {} %".format(100*(len(ftweets)-len(nftweets)-len(pftweets))/len(ftweets)))
+    else:
+        print("\nERROR: No female tweets")
     # picking male names from tweets 
+
     mtweets = [tweet for tweet in tweets if tweet['gender'][0] == 'male'] 
     pmtweets = [tweet for tweet in mtweets if tweet['sentiment'] == 'positive']
     nmtweets = [tweet for tweet in mtweets if tweet['sentiment'] == 'negative']
-    # percentage of male names 
-    print("Male users percentage: {} %".format(100*len(mtweets)/len(tweets))) 
-    print("Male positive reviews percentage: {} %".format(100*len(pmtweets)/len(mtweets))) 
-    print("Male negative reviews percentage: {} %".format(100*len(nmtweets)/len(mtweets))) 
-    print("Male neutral reviews percentage: {} %".format(100*(len(mtweets)-len(nmtweets)-len(pmtweets))/len(mtweets)))
+    if len(mtweets)!=0:
+        # percentage of male names 
+        print("\nMale users percentage: {} %".format(100*len(mtweets)/len(tweets))) 
+        print("Male positive reviews percentage: {} %".format(100*len(pmtweets)/len(mtweets))) 
+        print("Male negative reviews percentage: {} %".format(100*len(nmtweets)/len(mtweets))) 
+        print("Male neutral reviews percentage: {} %".format(100*(len(mtweets)-len(nmtweets)-len(pmtweets))/len(mtweets)))
+    else:
+        print("ERROR: No male tweets")
     # percentage of unknown names 
-    print("Uknonwn names percentage: {} %".format(100*(len(tweets)-len(ftweets)-len(mtweets))/len(tweets)))
+    print("\nUknonwn names percentage: {} %".format(100*(len(tweets)-len(ftweets)-len(mtweets))/len(tweets)))
     # # printing first 5 positive tweets 
     # print("\n\nPositive tweets:") 
     # for tweet in ptweets[:10]: 
@@ -194,7 +220,9 @@ def main(data_dir,query,count=100,Save=True,load=False):
     #      print(tweet['text']) 
   
 if __name__ == "__main__":
-    data_dir= 'data'
     # query=input("Enter Query:\n")
-    query='good luck'
+    query='brunch'
+    t1=datetime.datetime.now()
     main(data_dir,query)
+    t2=datetime.datetime.now()
+    print("\nQuery Time: " + str((t2-t1).seconds) + 's\n')
